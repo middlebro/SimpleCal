c1583f76f71f052ba274844fcb269afab879d8bb
middlebro <middlebro@inha.edu>
2018-05-25 20:14:07 +0900

    Initial commit

---
+++ SimpleCal/source.cpp
@@ -0,0 +1,35 @@
+//
+//  source.cpp
+//  SimpleCal
+//
+//  Created by 서형중 on 2018. 5. 25..
+//
+
+int Implement(int A, int B);
+int Substraction(int A, int B);
+int Multiplication(int A, int B);
+int Division(int A, int B);
+int Remainder(int A, int B);
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    int A, B
+    char Op
+    cin >> A >> Op >> B;
+    if (Op == '+') {
+        Implement(A,B)
+    }
+    
+    return 0;
+}
+
+int Implement(int A, int B)
+{
+    cout << A + B << endl;
+}
+int Substraction(int A, int B);
+int Multiplication(int A, int B);
+int Division(int A, int B);
+int Remainder(int A, int B);
